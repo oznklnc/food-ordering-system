aae36c1e3efdd1e8f682de4cb91abf750545cfa4
ozankilinc <ozankilinc42@gmail.com>
2024-11-22 22:04:16 +0300

    create food ordering application

--- restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/BeanConfiguration.java
+++ restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/BeanConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BeanConfiguration {
 
+
     @Bean
     public RestaurantDomainService restaurantDomainService() {
         return new RestaurantDomainServiceImpl();
